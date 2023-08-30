b740f96994fd3b39c8cd7bf945861c37a31583ee
Vivian Desktop <visasal88@gmail.com>
2023-08-30 19:05:17 -0300

    feat: implement error handling in create method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,24 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
   create(req, res) {
     const { name, email, password } = req.body
+
+    if(!name){
+      throw new AppError("Nome é obrigatório!")
+    }
+
+    if(!email) {
+      throw new AppError("E-mail é obrigatório!")
+    }
+
+    if(!password) {
+      throw new AppError("Senha é obrigatório!")
+    }
+
+    if (password.length < 6){
+      throw new AppError("É necessário que a senha contenha pelo menos 6 caracteres!")
+    }
 
     res.status(201).json({ name, email, password })
   }
