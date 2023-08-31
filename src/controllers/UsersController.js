ae14999e4ff26cda829eb24f60b751e637ad5cc6
Vivian Desktop <visasal88@gmail.com>
2023-08-31 17:35:22 -0300

    feat: add password encryption with bcryptjs

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,6 @@
 const AppError = require("../utils/AppError")
 const sqlConnection = require("../database/sqlite")
+const { hash } = require("bcryptjs")
 
 class UsersController {
   async create(req, res) {
@@ -28,9 +29,11 @@ class UsersController {
       throw new AppError("É necessário que a senha contenha pelo menos 6 caracteres!")
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await database.run(
       "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, password]
+      [name, email, hashedPassword]
     )
 
     return res.status(201).json()
