cee3c5d82daf4614c00293201ff281813ee764b1
Vivian Desktop <visasal88@gmail.com>
2023-09-01 13:32:55 -0300

    feat: create route for show method

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -5,5 +5,6 @@ const dishesRoutes = Router()
 const dishesController = new DishesController()
 
 dishesRoutes.post("/:user_id", dishesController.create)
+dishesRoutes.get("/:id", dishesController.show)
 
 module.exports = dishesRoutes
