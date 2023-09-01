acdf70cb4a1a6ad851b09c3973cee7b8563848bd
Vivian Desktop <visasal88@gmail.com>
2023-09-01 13:28:02 -0300

    feat: add dishes.routes to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const { Router } = require("express")
 const routes = Router()
 
 const usersRouter = require("./users.routes")
+const dishesRouter = require("./dishes.routes")
 
 routes.use("/users", usersRouter)
+routes.use("/dishes", dishesRouter)
 
 module.exports = routes
