1eae7373175d9e32a24dd6e37a7e1e7cffa2887f
Vivian Desktop <visasal88@gmail.com>
2023-09-01 18:05:17 -0300

    feat: create route for index method

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -5,6 +5,7 @@ const dishesRoutes = Router()
 const dishesController = new DishesController()
 
 dishesRoutes.post("/:user_id", dishesController.create)
+dishesRoutes.get("/", dishesController.index)
 dishesRoutes.get("/:id", dishesController.show)
 dishesRoutes.delete("/:id", dishesController.delete)
 
