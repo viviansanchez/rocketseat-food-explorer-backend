b86aa2bfdca687ef481d377e27647f2b149ea7d1
Vivian Desktop <visasal88@gmail.com>
2023-09-14 19:48:10 -0300

    add middleware to dishes routes to ensure authentication

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express")
 const DishesController = require("../controllers/DishesController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const dishesRoutes = Router()
 const dishesController = new DishesController()
 
-dishesRoutes.post("/:user_id", dishesController.create)
+dishesRoutes.use(ensureAuthenticated)
+
+dishesRoutes.post("/", dishesController.create)
 dishesRoutes.put("/:id", dishesController.update)
 dishesRoutes.get("/", dishesController.index)
 dishesRoutes.get("/:id", dishesController.show)
