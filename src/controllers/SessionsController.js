0dfbd2ad5a09d8ef1e8cb000db1a78a6bc1027e9
Vivian Desktop <visasal88@gmail.com>
2023-09-14 17:42:53 -0300

    feat: add user and password validation, add jwt token generatior

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,32 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
+const authConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken")
+
 class SessionsController {
   async create(req, res) {
     const { email, password } = req.body
 
-    return res.json({ email, password })
+    const user = await knex("users").where({ email }).first()
+
+    if(!user) {
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if(!passwordMatched){
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+
+    const { expiresIn, secret } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id), 
+      expiresIn
+    })
+    
+    return res.json({ user, token })
   }
 }
 
