3b31b4dfab9a5e9f872577b4251206726974521a
Vivian Desktop <visasal88@gmail.com>
2023-09-14 16:57:00 -0300

    feat: create sessions.routes and add it to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const routes = Router()
 
 const usersRouter = require("./users.routes")
 const dishesRouter = require("./dishes.routes")
+const sessionsRoutes = require("./sessions.routes")
 
 routes.use("/users", usersRouter)
 routes.use("/dishes", dishesRouter)
+routes.use("/sessions", sessionsRoutes)
 
 module.exports = routes
