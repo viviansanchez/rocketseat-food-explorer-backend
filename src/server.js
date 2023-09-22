1db1bcf6179507a682cf5e94a2980e4b6195a4ff
Vivian Desktop <visasal88@gmail.com>
2023-09-22 18:50:46 -0300

    fix: add missing path to serve images to frontend app

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const AppError = require("./utils/AppError")
 const express = require("express")
 const routes = require("./routes")
 const cors = require("cors")
+const uploadConfig = require("./configs/upload")
 
 migrationsRun()
 
@@ -12,7 +13,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+
 app.use(routes)
+
 
 app.use((error, req, res, next) => {
   if(error instanceof AppError) {
